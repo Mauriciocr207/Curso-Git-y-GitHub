414e079338f6feff40b7b37e50f5e08350ca0282
Mauricio CR <mauriciocr123@outlook.com>
2023-04-13 00:03:42 -0600

    se agrega clase persona

--- app.js
+++ app.js
@@ -2,3 +2,13 @@
     const welcome = 'hola mundo!';
     console.log(welcome);
 })();
+
+class Persona {
+    constructor(nombre, apellido) {
+       this.nombre = nombre,
+       this.apellido = apellido
+    }
+    saludar() {
+        console.log('Hola a todos');
+    }
+}
