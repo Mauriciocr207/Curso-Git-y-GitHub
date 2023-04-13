1156b4ef3c430c45c8b86194c06fd570f89dbcda
Mauricio CR <mauriciocr123@outlook.com>
2023-04-13 00:08:15 -0600

    se crea script para clase persona

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
+import { Persona } from "./persona";
+
 (() => {
     const welcome = 'hola mundo!';
     console.log(welcome);
 })();
 
-class Persona {
-    constructor(nombre, apellido) {
-       this.nombre = nombre,
-       this.apellido = apellido
-    }
-    saludar() {
-        console.log('Hola a todos');
-    }
-}
---
+++ persona.js
@@ -0,0 +1,9 @@
+export class Persona {
+    constructor(nombre, apellido) {
+       this.nombre = nombre,
+       this.apellido = apellido
+    }
+    saludar() {
+        console.log('Hola a todos');
+    }
+}
