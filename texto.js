9eac4e0f495f10949ea452dc186af68e12cfbd20
Mauricio CR <mauriciocr123@outlook.com>
2023-04-13 00:22:22 -0600

    se añade libro

--- texto.js
+++ texto.js
@@ -1,5 +1,6 @@
 export const data = {
     'libro 1': 'Peter Pan',
     'libro 2': 'Harry Potter y las reliquias de la muerte',
-    'libro 3': 'Orgullo y Prejuicio'
+    'libro 3': 'Orgullo y Prejuicio',
+    'libro 4': 'El principito'
 }
