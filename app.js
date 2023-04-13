0d1e8cb811850cdf9a7c86ae09e2720c6eada6ba
Mauricio CR <mauriciocr123@outlook.com>
2023-04-13 00:15:30 -0600

    se añaden clases y funciones

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+import { sum } from "./sum";
+import { Persona } from "./persona";
+
 (() => {
     const welcome = 'hola mundo!';
     console.log(welcome);
     console.log(getSum([1,2,3]));
+    const persona1 = new Persona('Carlos', 'Pan');
+    console.log(`Hola, soy ${persona1.nombre} ${persona1.apellido} y 1 + 5 es: ${sum([1,5])}`);
+
 })();
